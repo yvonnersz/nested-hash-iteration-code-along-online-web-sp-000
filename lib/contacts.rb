7428432ee2729b916be9d632e8ba984f434aa5ef
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-31 08:38:59 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,19 +14,14 @@ require 'pry'
     }
   }
 
-# def remove_strawberry(contacts)
-#   contacts.each do |person,contact_details_hash|
-#     if person == "Freddy Mercury"
-#       contact_details_hash.each do |attribute,data|
-#         if attribute == :favorite_ice_cream_flavors
-#           data.delete_if {|ice_cream| ice_cream == "strawberry"}
-#         end
-#       end
-#     end
-#   end
-# end
-
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].shift()
-  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].join(" ")
+  contacts.each do |person,contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute,data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
